b2e605ebbfdba2802610f1d2318ffd460679706e
Ikr <santanaikaru@gmail.com>
2026-01-27 16:28:37 -0300

    Add game formatting and database management methods

--- src/lib/game.hpp
+++ src/lib/game.hpp
@@ -5,6 +5,8 @@
 #include "kvector.hpp"
 #include "result.hpp"
 #include <string>
+#include "parser.hpp"
+
 
 struct Game
 {
@@ -41,9 +43,66 @@ struct Game
     }
 };
 
+std::string format_game_st(Game game){
+    std::string price = std::to_string(game.price);
+    std::string avaliation = std::to_string(game.avaliation);
+    std::string year = std::to_string(game.year);
+    std::string id = std::to_string(game.id);
+    return game.name + ";" + game.publisher + ";" + price + ";" + avaliation + ";" + year + ";" + id + "\n";
+}
+
+
+
 class ListGames{
     public:
         ListGames(){};
+
+        Result<std::string, std::string> init_db(std::string path){
+            Result<std::string, std::string> status; 
+            std::ifstream file(path);
+            std::string buffer;
+            if (file.bad()){
+                file.close();
+                status.err("Error type bad file!");
+                return status;
+            }
+            // Descarta o header csv do arquivo
+            std::getline(file, buffer);
+            buffer = "";
+            while(std::getline(file, buffer)){
+                Kvector<std::string> tokens;
+                tokens = split_string(buffer, ';');
+                if (tokens.len() == 6){
+                    Game game(
+                        tokens[0],
+                        tokens[1],
+                        std::stof(tokens[2]),
+                        std::stoi(tokens[3]),
+                        std::stoi(tokens[4]),
+                        std::stoi(tokens[5])
+                    );
+                    list.push(game);
+                }
+            }   
+            status.ok("initialization completed!");
+            file.close();
+            return status;
+        }
+        Result<std::string, std::string> save_db(std::string path){
+            Result<std::string, std::string> status;
+            std::ofstream file(path);
+            std::string header = "# name; publisher; price; avaliation; year; id\n";
+            if (file.bad()){
+                status.err("Error type bad file!");
+                file.close();
+                return status;
+            }
+            file << header;
+            for(size_t i = 0; i < list.len(); i++){
+                file << format_game_st(list[i]);
+            }
+            status.ok("save completed!");
+        }
 
         void add_game(Game game){
             list.push(game);
@@ -59,4 +118,7 @@ class ListGames{
         Kvector<Game> list;
 };
 
-#endif GAME_HPP
+
+
+
+#endif 
